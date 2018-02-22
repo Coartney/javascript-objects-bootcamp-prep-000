5724328aedd52df1bc5fd38df642e4c9643ad137
coartney22@gmail.com <coartney22@gmail.com>
2018-02-22 00:52:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName){
-  key = artistName'
+  key = artistName;
   delete playlist.key;
   return playlist;
 }
